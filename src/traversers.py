6a22a36cf11c1b6968d6f119133cbbc1410bef8d
slicerrr0 <mrslicerr0@protonmail.com>
2023-04-06 00:42:10 -0400

    created consrtructor method for `Node` object

--- src/traversers.py
+++ src/traversers.py
@@ -2,3 +2,16 @@
 This module contains classes used to represent the hierarchical
 structure of a Symphony's code.
 '''
+
+class Node:
+    '''
+    Base class for reprsenting logic nodes in a Symphony's code.
+    '''
+    def __init__(self, parent=None) -> None:
+        if parent is not None and not isinstance(parent, Node):
+            raise ValueError(f'''
+                Parmeter {parent} was of type {type(parent)}
+                Constructor method expected this paremter to be a
+                Node or a NoneType object.
+            ''')
+        self.parent = parent
