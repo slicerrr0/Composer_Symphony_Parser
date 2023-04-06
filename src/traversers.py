c1f7601d088f05190a312c64a68ecae7cae0b76f
slicerrr0 <mrslicerr0@protonmail.com>
2023-04-06 00:49:33 -0400

    created child Node classes that inherit from `BaseNode`

--- src/traversers.py
+++ src/traversers.py
@@ -4,3 +4,19 @@ structure of a Symphony's code.
 '''
 
 from .base import BaseNode
+
+class ConditionalNode(BaseNode):
+    def __init__(self, parent=None|BaseNode) -> None:
+        super().__init__(parent)
+
+class FilterNode(BaseNode):
+    def __init__(self, parent=None|BaseNode) -> None:
+        super().__init__(parent)
+
+class AssetNode(BaseNode):
+    def __init__(self, parent=None|BaseNode) -> None:
+        super().__init__(parent)
+
+class WeightNode(BaseNode):
+    def __init__(self, parent=None|BaseNode) -> None:
+        super().__init__(parent)
