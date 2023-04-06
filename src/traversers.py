d99e1ea68864d946a9f116dd2c754432c13c4a6a
slicerrr0 <mrslicerr0@protonmail.com>
2023-04-06 00:54:17 -0400

    added docstring for `ConditionalNode` class

--- src/traversers.py
+++ src/traversers.py
@@ -6,6 +6,12 @@ structure of a Symphony's code.
 from .base import BaseNode
 
 class ConditionalNode(BaseNode):
+    '''
+    Conditional nodes function as switches between multiple
+    logic paths, the path that the algorithm takes dependent
+    on the boolean evaluation of the expression associated
+    with the conditional node.
+    '''
     def __init__(self, parent=None|BaseNode) -> None:
         super().__init__(parent)
 
