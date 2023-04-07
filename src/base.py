4fcb4e001b3dcc3c4191899d32d46fb9f85938fa
slicerrr0 <mrslicerr0@protonmail.com>
2023-04-07 00:19:47 -0400

    created `parse_rebalance_threshold' and accompanying regex pattern

--- src/base.py
+++ src/base.py
@@ -59,5 +59,10 @@ class BaseParser:
     '''
     def __init__(self, text: str) -> None:
         self.text = text
-
-    
+        self.REBLANCE_PATTERN = r'(?<={:rebalance-threshold )[^}]+'
+    def parse_rebalance_threshold(self):
+        '''
+        Parses `self.text` for a match to `self.REBALANCE_PATTERN`. This 
+        match corresponds to the rebalance threshold set for the Symphony.
+        '''
+        return re.search(self.REBLANCE_PATTERN, self.text).group(0)
